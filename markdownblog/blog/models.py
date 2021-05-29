7504ed41d3411eafcc94a422d22bf974331c4189
Sergei <comp.it.05@inbox.ru>
2021-05-29 16:51:42 +0300

    the simplest blog with markdown

---
+++ markdownblog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
